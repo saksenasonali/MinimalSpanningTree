8003c69965fbe7172c9d58cbfba15cff2f510aaa
Samarth Jain <gosamarth@gmail.com>
2018-08-24 11:39:19 +0530

    Updated Graph Generator for Connected graph

--- graphGen.py
+++ graphGen.py
@@ -1,9 +1,21 @@
 import random
 
-nodeCount = 6
-edgeCount = 10
+nodeCount = 10
+edgeCount = 20
+
+print (nodeCount, edgeCount)
 
 edges = {}
+
+while len(edges) < nodeCount:
+    x = len(edges) + 1
+    y = random.randint (1, nodeCount)
+    if x == y: continue
+    #comment the following line if the graph is directed
+    if y < x: x, y = y, x
+    w = random.randint(1, 100)
+    edges [x, y] = w
+
 while len(edges) < edgeCount:
     x = random.randint (1, nodeCount)
     y = random.randint (1, nodeCount)
